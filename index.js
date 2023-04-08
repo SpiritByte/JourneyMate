27f1aff47c503efb9bab34a6dbc59b50250fb2dd
panthk <124208649+panthk@users.noreply.github.com>
2023-04-08 11:49:45 -0400

    take two

--- index.js
+++ index.js
@@ -1,57 +1,71 @@
 require('dotenv').config();
 const axios = require('axios');
-const { Client, GatewayIntentBits } = require('discord.js')
+const { Client, MessageEmbed, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        // ...
-    ]
+  intents: [
+      GatewayIntentBits.Guilds,
+      // ...
+  ]
 })
 
 client.once('ready', () => {
   console.log('JourneyMate Jr. is ready!');
 });
 
-client.on('interactionCreate', async interaction => {
+client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
 
   if (interaction.commandName === 'search') {
     const query = interaction.options.getString('query');
-    await interaction.reply({ content: `Searching for tourist attractions related to "${query}"...`, ephemeral: true });
-    
-    const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-      prompt: `What are the top 5 tourist attractions in ${query}? Add a description of each.`,
-      max_tokens: 2000,
-      n: 1,
-      stop: '###'
-    }, {
-      headers: {
-        'Authorization': `Bearer ${process.env.API_TOKEN}`,
-        'Content-Type': 'application/json'
-      }
+    await interaction.reply({
+      content: `Searching for tourist attractions related to "${query}"...`,
+      ephemeral: false,
     });
-    
-    
-    
-    console.log(response.data.choices)
-    const result = response.data.choices[0].text;
-    //const result = response.data.choices[0].text.trim();
-    
+
+    const response = await axios.post(
+      'https://api.openai.com/v1/engines/text-davinci-003/completions',
+      {
+        prompt: `What are the top 5 tourist attractions in ${query}? Add a description of each.`,
+        max_tokens: 200,
+        n: 1,
+        stop: '###',
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.API_TOKEN}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    console.log(response.data.choices);
+    const result = response.data.choices[0].text.trim();
+
     if (result === '') {
-      //await interaction.editReply({ content: `No results found for "${query}".`, ephemeral: true });
-      console.log("no result")
+      await interaction.editReply({ content: `No results found for "${query}".`, ephemeral: false });
     } else {
-      await interaction.editReply({ content: result, ephemeral: true });
+      const embed = new EmbedBuilder()
+        .setColor('#0099ff')
+        .setTitle(`Tourist attractions in ${query}`)
+        .setDescription(result)
+        .setTimestamp();
+      
+      await interaction.editReply({content: "", embeds: [embed], ephemeral: false });
     }
   }
 });
 
 client.on('ready', async () => {
-  const guildId = '1093941116816072796';
-  const guild = await client.guilds.cache.get(guildId);
+  const guildId = process.env.GUILD_ID;
+  const clientId = process.env.CLIENT_ID;
+  const clientSecret = process.env.CLIENT_SECRET;
+  const botToken = process.env.BOT_TOKEN;
+  const apiToken = process.env.API_TOKEN;;
+
+  const guild = client.guilds.cache.get(guildId);
   if (!guild) {
     console.log(`Unable to fetch guild with ID ${guildId}`);
     return;
@@ -64,22 +78,22 @@ client.on('ready', async () => {
       options: [
         {
           name: 'query',
-          type: 3,
+          type: 'STRING',
           description: 'The location to search for',
           required: true,
-        }
-      ]
-    }
+        },
+      ],
+    },
   ];
 
-  const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
+  const rest = new REST({ version: '9' }).setToken(botToken);
 
   try {
     console.log('Started refreshing application (/) commands.');
 
     await rest.put(
-      Routes.applicationGuildCommands(client.user.id, guildId),
-      { body: commands },
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands }
     );
 
     console.log('Successfully reloaded application (/) commands.');
